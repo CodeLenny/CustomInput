4e3d4a7a2a43252153b5ded34f8fae7b24a9095d
Ryan Leonard <rhinoisnot@gmail.com>
2015-02-15 10:18:26 -0500

    Fixed problematic quotes.

--- build/example/truefalse/TrueFalse.js
+++ build/example/truefalse/TrueFalse.js
@@ -11,9 +11,9 @@
   requirejs.config({
     baseUrl: baseUrl,
     paths: {
-      CustomInput: "//cdn.rawgit.com/CodeLenny/CustomInput/" + version + "/build/",
-      TrueFalse: "//cdn.rawgit.com/CodeLenny/CustomInput/" + version + "/build/example/truefalse/",
-      requirejs: '//cdn.rawgit.com/jrburke/requirejs/49086cf903410c746140fd968151a8aa95cc3482/',
+      CustomInput: "//cdn.rawgit.com/CodeLenny/CustomInput/" + version + "/build",
+      TrueFalse: "//cdn.rawgit.com/CodeLenny/CustomInput/" + version + "/build/example/truefalse",
+      requirejs: '//cdn.rawgit.com/jrburke/requirejs/49086cf903410c746140fd968151a8aa95cc3482',
       jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min"
     }
   });
