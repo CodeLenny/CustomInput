3d1a63897a4d96f9fb5d0a31f566b4458c9beb10
Ryan Leonard <rhinoisnot@gmail.com>
2015-02-16 14:47:24 -0500

    Updated version.

--- build/example/truefalse/TrueFalse.js
+++ build/example/truefalse/TrueFalse.js
@@ -6,7 +6,7 @@
 
   inputVersion = "41b6dbe";
 
-  exampleVersion = "13465f4";
+  exampleVersion = "fff4de2";
 
   requirejs.config({
     baseUrl: "//cdn.rawgit.com/CodeLenny/CustomInput/" + inputVersion + "/build",
