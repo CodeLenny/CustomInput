16046d51c6be365c8b2891dc6ae6a99104427f3a
Ryan Leonard <rhinoisnot@gmail.com>
2015-02-16 09:50:25 -0500

    Fixed syntax.

--- build/Editor.js
+++ build/Editor.js
@@ -25,7 +25,7 @@
         menu.append(header);
         _results = [];
         for (x = _i = 0, _ref = types.length; 0 <= _ref ? _i < _ref : _i > _ref; x = 0 <= _ref ? ++_i : --_i) {
-          type = types.get(x);
+          type = types[x];
           item = $("<div />").addClass("goog-menuitem apps-menuitem").attr({
             id: ":" + this.main.prefix + "insert" + x,
             role: "menuitem",
